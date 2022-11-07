7b13c34b0512c24c20310553db179c5fbe8ef0c3
dgibb <dgibb802@gmail.com>
2022-11-06 22:40:08 -0500

    web stuff

---
+++ web/components/mychart.vue
@@ -0,0 +1,108 @@
+<template>
+    <client-only>
+        <div class="flex margin-bottom">
+            <svg :class="data.name"></svg>
+            <button @click="onClick()">click me</button>
+        </div>
+    </client-only>
+</template>
+
+<script>
+import * as d3 from 'd3'
+
+export default {
+    name: 'IndexPage',
+    props: {
+        data: {
+            type: Object,
+            default: () => {return {}} 
+        }
+    },
+    methods: {
+        onClick() {
+            console.log("HERE")
+               const width = 500;
+            const height = 200;
+
+            const data = this.data.data.slice(0, 100);
+            console.log(data)
+
+            let id = "." + this.data.name
+            const svg = d3.select(id).attr("width", width).attr("height", height);
+            const g = svg.append("g");
+
+            //2. Parse the dates
+            const parseTime = d3.utcParse("%Y-%m-%dT%H:%M:%S.%LZ");
+
+            //3. Creating the Chart Axes
+            const x = d3
+            .scaleTime()
+            .domain(
+                d3.extent(data, function (d) {
+                return parseTime(d.date);
+                })
+            )
+            .rangeRound([0, width]);
+
+            const y = d3
+            .scaleLinear()
+            .domain(
+                d3.extent(data, function (d) {
+                return d.amount;
+                })
+            )
+            .rangeRound([height, 0]);
+
+            //4. Creating a Line
+            const line = d3
+            .line()
+            .x(function (d) {
+                return x(parseTime(d.date));
+            })
+            .y(function (d) {
+                return y(d.amount);
+            });
+
+            //5. Appending the Axes to the Chart
+            g.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x));
+
+            g.append("g")
+            .call(d3.axisLeft(y))
+            .append("text")
+            .attr("fill", "#000")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", "0.71em")
+            .attr("text-anchor", "end")
+            .text("Price ($)");    
+
+            //6. Appending a path to the Chart
+            g.append("path")
+            .datum(data)
+            .attr("fill", "none")
+            .attr("stroke", this.data.color)
+            .attr("stroke-width", 1.5)
+            .attr("d", line);
+
+        }
+    },
+}
+</script>
+
+<style>
+svg {
+    display: block;
+    margin: auto;
+    border: 3px solid #F0F8FF;
+}
+
+.flex {
+    display: flex;
+}
+
+.margin-bottom {
+    margin-bottom: 50px;
+}
+</style>
---
+++ web/pages/index.vue
@@ -0,0 +1,82 @@
+<template>
+    <div>
+        <heady/>
+            <div :class="'sum-padding'">
+                <div id="chart">
+                    <client-only>
+                        <mychart :data="temp" :ident="name"></mychart>
+                        <mychart :data="ph"></mychart>
+                        <mychart :data="tb"></mychart>
+                        <mychart :data="flow"></mychart>
+                        <mychart :data="cdt"></mychart>
+                    </client-only>
+                </div>
+            </div>
+        <footy/>
+    </div>
+</template>
+
+<script>
+import heady from '../components/header';
+import footy from '../components/footer';
+import mychart from '../components/mychart';
+import testData from '~/data/testdata'; 
+
+export default {
+  name: 'IndexPage',
+  components: {
+    heady, 
+    footy,
+    mychart,
+  },
+  
+  data(){
+        return{
+            p: testData,
+        }
+    },
+    computed: {
+        temp() {
+            return { 
+                name: "Temperature",
+                color: "Red",
+                data: this.p.map(function (el) { return {date: el.time, amount: el.temp}})
+            }
+        },
+        ph() {
+            return { 
+                name: "PH",
+                color: "Blue",
+                data: this.p.map(function (el) {  return {date: el.time, amount: el.ph}})
+            }
+        },
+        tb() {
+            return { 
+                name: "Turbidity",
+                color: "Green",
+                data: this.p.map(function (el) {  return {date: el.time, amount: el.tb} })
+            }
+        },
+        flow() {
+            return { 
+                name: "Flow",
+                color: "Yellow",
+                data: this.p.map(function (el) { return { date: el.time, amount: el.flow} })
+            }
+        },
+        cdt() {
+            return { 
+                name: "Conductivity",
+                color: "Purple",
+                data: this.p.map(function (el) { return {date: el.time, amount: el.cdt}})
+            }
+        },
+    } 
+}
+</script>
+
+<style>
+.sum-padding {
+    padding-top: 100px;
+}
+</style>
