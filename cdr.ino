68404bd224b0524c52589d14e75bc8ec1550a156
PatrickDenert <pdenert@gmail.com>
2023-02-27 11:30:05 -0500

    Add files via upload

---
+++ cdr.ino
@@ -0,0 +1,202 @@
+#include <OneWire.h>
+#include <tinyECC.h>
+#define TdsSensorPin A1
+#define VREF 5.0      // analog reference voltage(Volt) of the ADC
+#define SCOUNT  30           // sum of sample point
+int analogBuffer[SCOUNT];    // store the analog value in the array, read from ADC
+int analogBufferTemp[SCOUNT];
+int analogBufferIndex = 0,copyIndex = 0;
+float averageVoltage = 0,tdsValue = 0;
+int DS18S20_Pin = 2; //DS18S20 Signal pin on digital 2
+volatile double waterFlow;
+
+//Temperature chip i/o
+OneWire ds(DS18S20_Pin);  // on digital pin 2one
+
+void setup(void) {
+  Serial.begin(9600);                 //set baudrate
+  pinMode(TdsSensorPin,INPUT);
+  waterFlow = 0;
+  attachInterrupt(1, pulse, RISING);  //set interrupt pin to d3, call pulse() on interrupt, call interrupt when pin goes L->H
+  
+}
+
+void loop(void) {
+  float temperature = 0;
+  float turbidity = 0;
+  float tds = 0;
+  
+  for(int i = 0; i<100; i++){                //take lots of samples of measurements
+    temperature = temperature + getTemp();
+    turbidity = turbidity + getTurbidity();
+    tds = tds + getTDS(temperature/i);
+  }
+  
+  temperature = temperature/100;            //get avg temp measuremnt
+  turbidity = turbidity/100;                //get avg turbidiy measuremnt
+  tds = tds/100;                            //get avg tds measurement
+  float conductivity = tds*2;
+  float tempF = temperature*1.8 + 32;
+
+  String labels[] = {"temperature: ", "temperature: ", "turbidity: ", "tds: ", "conductivity: ", "waterflow: "};
+  float parameters[] = {temperature, tempF, turbidity, tds, conductivity, waterFlow};
+  String units[] = {" Celsius", " Farenheight", " NTU", " ppm", " us/cm", " L/s"};
+ 
+  Serial.println("--------------------------------");
+  for (int i = 0 ; i < 5 ; i = i + 1){
+    Serial.print(labels[i]);
+    Serial.print(parameters[i]);
+    Serial.println(units[i]);
+  }
+
+  tinyECC e;
+  e.plaintext = "test";
+  e.encrypt();                   // Encrypt plaintext and store result in ciphertext
+  e.genSig();                   // Generate Signature
+  Serial.println(e.ciphertext); // Display ciphertext
+  Serial.println(String(e.Sig[0])+","+String(e.Sig[1])); // Display generated Signature (Signature is a 2-element array)
+
+
+
+  delay(500);  //pause arduino
+  
+}
+
+float alert(float tds, float temperature, float turbidity, float conductivity){
+  if(tds > 500){
+    Serial.print("Warning: tds levels past maximum limit!");
+    return 0;
+  }
+
+  if(temperature > 40){
+    Serial.print("Warning: temperature past maximum limit!");
+    return 0;
+  }
+
+  if(turbidity > 1){
+    Serial.print("Warning: turbidity past maximum limit!");
+    return 0;
+  }
+
+  if(conductivity > 1000){
+    Serial.print("Warning: turbidity past maximum limit!");
+    return 0;
+  }
+  Serial.print("All metrics within safe values");
+  return 0;
+  }
+
+float getTDS(float temperature){
+  static unsigned long analogSampleTimepoint = millis();
+   if(millis()-analogSampleTimepoint > 40U)     //every 40 milliseconds,read the analog value from the ADC
+   {
+     analogSampleTimepoint = millis();
+     analogBuffer[analogBufferIndex] = analogRead(TdsSensorPin);    //read the analog value and store into the buffer
+     analogBufferIndex++;
+     if(analogBufferIndex == SCOUNT) 
+         analogBufferIndex = 0;
+   }
+   static unsigned long printTimepoint = millis();
+   if(millis()-printTimepoint > 800U)
+   {
+      printTimepoint = millis();
+      for(copyIndex=0;copyIndex<SCOUNT;copyIndex++)
+        analogBufferTemp[copyIndex]= analogBuffer[copyIndex];
+      averageVoltage = getMedianNum(analogBufferTemp,SCOUNT) * (float)VREF / 1024.0; // read the analog value more stable by the median filtering algorithm, and convert to voltage value
+      float compensationCoefficient=1.0+0.02*(temperature-25.0);    //temperature compensation formula: fFinalResult(25^C) = fFinalResult(current)/(1.0+0.02*(fTP-25.0));
+      float compensationVolatge=averageVoltage/compensationCoefficient;  //temperature compensation
+      tdsValue=(133.42*compensationVolatge*compensationVolatge*compensationVolatge - 255.86*compensationVolatge*compensationVolatge + 857.39*compensationVolatge)*0.5; //convert voltage value to tds value
+   }
+   return tdsValue;   
+  
+}
+float getTurbidity(){
+  int sensorValue = analogRead(A0);// read the input on analog pin 0:
+  float volt = sensorValue * (5.0 / 1024.0); // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+  if(volt < 2.5){
+    return 3000;
+  }
+  if(volt > 4.2){
+    return 0;
+  }
+  float turbidity = -1120.4*volt*volt + 5742.3*volt - 4352.9;
+
+  return turbidity;
+}
+
+float getTemp(){
+  //returns the temperature from one DS18S20 in DEG Celsius
+
+  byte data[12];
+  byte addr[8];
+
+  if ( !ds.search(addr)) {        //get address of ds18b20
+      //no more sensors on chain, reset search
+      ds.reset_search();
+      return -1000;
+  }
+
+  if ( OneWire::crc8( addr, 7) != addr[7]) {    //get crc
+      Serial.println("CRC is not valid!");
+      return -1000;
+  }
+
+  if ( addr[0] != 0x10 && addr[0] != 0x28) {
+      Serial.print("Device is not recognized");
+      return -1000;
+  }
+
+  ds.reset();
+  ds.select(addr);   //choose correct temperature sensor
+  ds.write(0x44,1); // start conversion, with parasite power on at the end
+
+  byte present = ds.reset();      //1 = device is present
+  ds.select(addr);                //choose correct temperature sensor
+  ds.write(0xBE); // Read Scratchpad and crc
+
+
+  for (int i = 0; i < 9; i++) { // we need 9 bytes 
+    data[i] = ds.read();
+  }
+
+  ds.reset_search();            //reset between searches
+
+  byte MSB = data[1];           // msb of temp measuremtn
+  byte LSB = data[0];           //lsb of temp measurement
+
+  float tempRead = ((MSB << 8) | LSB); //using two's compliment //co,bine 2 numbers into 1
+  float TemperatureSum = tempRead / 16;     //convert temp reading
+
+  return TemperatureSum;
+
+}
+
+int getMedianNum(int bArray[], int iFilterLen) 
+{
+      int bTab[iFilterLen];
+      for (byte i = 0; i<iFilterLen; i++)
+      bTab[i] = bArray[i];
+      int i, j, bTemp;
+      for (j = 0; j < iFilterLen - 1; j++) 
+      {
+      for (i = 0; i < iFilterLen - j - 1; i++) 
+          {
+        if (bTab[i] > bTab[i + 1]) 
+            {
+        bTemp = bTab[i];
+            bTab[i] = bTab[i + 1];
+        bTab[i + 1] = bTemp;
+         }
+      }
+      }
+      if ((iFilterLen & 1) > 0)
+    bTemp = bTab[(iFilterLen - 1) / 2];
+      else
+    bTemp = (bTab[iFilterLen / 2] + bTab[iFilterLen / 2 - 1]) / 2;
+      return bTemp;
+}
+
+void pulse()   //measure the quantity of square wave
+{
+  waterFlow += 1.0 / 150.0; // 150 pulses=1L (refer to product specification）
+}
